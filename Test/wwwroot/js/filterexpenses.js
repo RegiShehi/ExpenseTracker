28fa3d90e4023ac96d4cf862a8a4e4f6cd1a0903
Regi Shehi <shehi.regi@gmail.com>
2020-02-16 20:36:00 +0100

    Add validation for dates in Expense Filter

--- Test/wwwroot/js/filterexpenses.js
+++ Test/wwwroot/js/filterexpenses.js
@@ -1,35 +1,58 @@
-﻿$(function () {
+﻿var dateFormat = "dd/mm/yy";
+
+$(function () {
+    $("#filteredAmount").text("0.00");
+
     var fromDate = $("#fromDatepicker").datepicker({
-        dateFormat: "dd/mm/yy"
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 1,
+        dateFormat: dateFormat
+    }).on("change", function () {
+        toDate.datepicker("option", "minDate", getDate(this));
+    });
+
+    var toDate = $("#toDatepicker").datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 1,
+        dateFormat: dateFormat
+    }).on("change", function () {
+        fromDate.datepicker("option", "maxDate", getDate(this));
     });
 
     fromDate.datepicker("setDate", new Date());
-
-    var toDate = $("#toDatepicker").datepicker({
-        dateFormat: "dd/mm/yy"
-    });
-
     toDate.datepicker("setDate", new Date());
 
-    $("#filteredAmount").text("0.00");
+    var form = $("#filterForm");
+    form.validate();
 
-    $(function () {
-        $('#submit').on('click', function (e) {
-            e.preventDefault();
+    $('#submit').on('click', function (e) {
+        e.preventDefault();
 
-            var form = $("#filterForm");
-            var isValid = form.valid();
+        var form = $("#filterForm");
+        var isValid = form.valid();
 
-            if (isValid) {
-                $.post('', form.serialize(), function (data) {
-                    var value = data.toLocaleString(undefined, {
-                        minimumFractionDigits: 2,
-                        maximumFractionDigits: 2
-                    });
-
-                    $("#filteredAmount").text(value);
+        if (isValid) {
+            $.post('', form.serialize(), function (data) {
+                var value = data.toLocaleString(undefined, {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2
                 });
-            }
-        });
+
+                $("#filteredAmount").text(value);
+            });
+        }
     });
 });
+
+function getDate(element) {
+    var date;
+    try {
+        date = $.datepicker.parseDate(dateFormat, element.value);
+    } catch (error) {
+        date = null;
+    }
+
+    return date;
+}
